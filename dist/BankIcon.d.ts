6f26fadb0a7375a7e99ed9e246cd3172cc42845c
dladncks1217 <dlaxodud1217@gmail.com>
2023-05-04 20:48:43 +0900

    build: npm 배포

--- dist/BankIcon.d.ts
+++
@@ -1,9 +0,0 @@
-import React from "react";
-type dataIconProps = {
-    dataName: string;
-    dataIcon: Record<string, string>;
-    determineType: React.Dispatch<React.SetStateAction<string>>;
-    selectType: React.Dispatch<React.SetStateAction<boolean>>;
-};
-declare const BankIcon: ({ dataName, dataIcon, determineType, selectType, }: dataIconProps) => JSX.Element;
-export default BankIcon;
